5d510f2387d512d193bb934edd2fd2c7f7265ade
Nichlos Incardone <nickincardone@gmail.com>
2017-01-04 09:09:11 -0500

    added sqlite db for alarms, (not working)

--- alarmer/alarm_clock.py
+++ alarmer/alarm_clock.py
@@ -1,26 +1,41 @@
+import time
+import sqlite3
+import os
+
 from datetime import datetime
 from threading import Thread
-import time
 from alarmer import light_switch
+
+dir = os.path.dirname(__file__)
+filename = os.path.join(dir, '..','db.sqlite')
+conn = sqlite3.connect(filename)
+
+def execute_query(query):
+    c = conn.cursor()
+    c.execute(query)
+    conn.commit()
+    return c
+
+def add_alarm(hour, minute, song='todo', weekdays):
+    execute_query("insert into alarms values ('{}','{}','{}','{}')".format(hour, minute, song, weekdays))
+
+def init():
+    execute_query('''CREATE TABLE IF NOT EXISTS alarms
+             (hour text, minute text, song text, weekdays text)''')
 
 def run_schedule():
     while 1:
-        check()
+        check_for_alarm()
         time.sleep(60)
 
 def run():
     t = Thread(target=run_schedule)
     t.start()
 
-def check():
+def check_for_alarm():
     #day of week where monday is 0
     if datetime.now().weekday() < 5 and datetime.now().hour == 20 and datetime.now().minute == 0:
-        print "yasss"
         light_switch.off()
-        return True
     if datetime.now().weekday() < 5 and datetime.now().hour == 8 and datetime.now().minute == 30:
-        print "yasss"
         light_switch.off()
-        return True
     print datetime.now()
-    return False
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from alarmer import alarm_clock
 
 if __name__ == '__main__':
-    alarm_clock.run()
+    alarm_clock.init()
+    #alarm_clock.run()
